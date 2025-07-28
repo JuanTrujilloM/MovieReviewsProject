791022c7dad187b939a418b9afc70f59808b58e4
Trujillo <jetrujillm@eafit.edu.co>
2025-07-28 18:34:26 -0500

    Refactored about view to use about.html template: - Created about.html file inside templates folder. - Updated about view in views.py to render the new template using Django's render function. - Added routing to display the about page at /about.

--- movie/views.py
+++ movie/views.py
@@ -9,4 +9,4 @@ def home(request):
     return render(request, 'home.html', {'name': 'Juan Esteban Trujillo Montes'})
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Us</h1>')
+    return render(request, 'about.html')
